cd679801cc808a73c3963c3ef1f4b840bad97edd
becky3 <rhythmcan@yahoo.co.jp>
2020-05-27 10:59:10 +0900

    - 敵の表示調整

--- model/character_chip.py
+++ model/character_chip.py
@@ -9,41 +9,35 @@ class CharacterChip():
     def __init__(self,
                  dot_size: (int, int),
                  area_size: (int, int),
-                 max_pattern: int = 2
+                 direction_pattern_num: int,
+                 animation_pattern_num: int = 2,
                  ):
         self.__chip = Chip(dot_size, area_size)
         self.__character_no = 0
-        self.__max_pattern = max_pattern
+        self.__direction_pattern_num = direction_pattern_num
+        self.__animation_pattern_num = animation_pattern_num
 
     def set_character_no(self, no: int):
         self.__character_no = no
 
-    def __get_direction_no(self, direction: int) -> int:
-        order = Direction.get_order(direction)
-        if order == -1:
-            order = 2
-        return order
-
     def get_draw_rect(self,
-                      direction: int = Direction.DOWN,
+                      direction_no: int = 0,
                       pattern: int = 0
                       ):
 
         chip_column = self.__chip.item_size[0]
-        max_pattern = self.__max_pattern
+        anime_pattern_num = self.__animation_pattern_num
 
-        max_oneline_char = chip_column / max_pattern
+        max_oneline_char = chip_column / anime_pattern_num
 
         character_no = self.__character_no
         char_row = math.floor(character_no / max_oneline_char)
         char_col = character_no % max_oneline_char
 
-        max_direction = 4
-        char_detail_row = char_row * max_direction
-        char_datail_col = char_col * max_pattern
-        direction_no = self.__get_direction_no(direction)
+        char_detail_row = char_row * self.__direction_pattern_num
+        char_datail_col = char_col * anime_pattern_num
         row = (char_detail_row + direction_no) * chip_column
-        col = char_datail_col + (pattern % max_pattern)
+        col = char_datail_col + (pattern % anime_pattern_num)
         index = row + col
 
         return self.__chip.get_draw_rect(index)
